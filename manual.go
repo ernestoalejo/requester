21ccb9a7a777358c613397f2af2032120d636590
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2012-12-27 17:59:16 +0100

    Enforce the manual reviews limit.

--- manual.go
+++ manual.go
@@ -4,7 +4,14 @@ import (
 	"log"
 )
 
+var manuals int
+
 func ManualRevision(action *Action) {
 	log.Printf("[%d] Manual revision required!\n", action.Id)
 	manualLogger.Printf("[%d] Manual revision required!\n", action.Id)
+
+	manuals++
+	if manuals >= config.MaxReviews {
+		log.Fatal("Too much manual reviews")
+	}
 }
