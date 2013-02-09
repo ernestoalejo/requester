cf6ab7656cbaba2517d2f826643a956f2f8dd664
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2013-02-10 00:38:46 +0100

    Add an algorithm to merge match sets for combined data.

--- response.go
+++ response.go
@@ -39,3 +39,68 @@ func (r *Response) ReList(re *regexp.Regexp) ([][]string, [][]int) {
 
 	return matchs, newpos
 }
+
+type Result struct {
+	Re  *regexp.Regexp
+	Len int
+}
+
+type ResultList struct {
+	matchs [][][]string
+	cur    int
+}
+
+func (l *ResultList) Next() bool {
+	l.cur++
+	return l.cur < len(l.matchs[0])
+}
+
+func (l *ResultList) Re(i int) []string {
+	if i < 0 || i >= len(l.matchs) {
+		panic("wrong re position")
+	}
+	return l.matchs[i][l.cur]
+}
+
+// Merge lists of matches and positions for a search.
+// The first result passed will be the base one, the reference for the
+// max length and the starting point of each section.
+// Then each one of the rest will be checked and assigned to one of that
+// sections according to it's starting position too.
+// The result it's an iterator you can use to access all the joined data
+// at once (it will return empty strings lists for the non-existent matchs).
+func (r *Response) MergeResults(results []*Result) (*ResultList, error) {
+	// Take the base positions
+	baseMatchs, basePos := r.ReList(results[0].Re)
+	AssertLen(baseMatchs, results[0].Len)
+
+	matchs := make([][][]string, len(results))
+	matchs[0] = baseMatchs
+
+	// Calculate the offsets for each of the other ones
+	for i, _ := range results {
+		if i == 0 {
+			continue
+		}
+
+		// Obtain the derivated list of items
+		ms, ps := r.ReList(results[i].Re)
+		AssertLen(ms, results[i].Len)
+		if len(ms) > len(baseMatchs) {
+			return nil, Errorf("more results for derivated list than in the base one")
+		}
+
+		// Merge the results with the base list
+		matchs[i] = make([][]string, len(baseMatchs))
+		cur := 0
+		for j, p := range ps {
+			for cur < len(basePos)-1 && basePos[cur+1][0] < p[0] {
+				matchs[i][j] = make([]string, results[0].Len)
+				cur++
+			}
+			matchs[i][cur] = ms[j]
+		}
+	}
+
+	return &ResultList{matchs: matchs, cur: -1}, nil
+}
