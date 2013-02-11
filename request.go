4c0831f4f4648a4fa6a4eca79071425920ca9c92
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2013-02-11 14:24:16 +0100

    POST requests shorthand method.

--- request.go
+++ request.go
@@ -1,7 +1,9 @@
 package requester
 
 import (
+	"bytes"
 	"net/http"
+	"net/url"
 )
 
 type Request struct {
@@ -12,6 +14,19 @@ type Request struct {
 
 func GET(url string) *Request {
 	r, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		// The URL was ill-formed. Exit directly, it's not a common error
+		panic(err)
+	}
+
+	return &Request{Req: r}
+}
+
+func POST(url string, values url.Values) *Request {
+	body := bytes.NewBuffer(nil)
+	body.WriteString(values.Encode())
+
+	r, err := http.NewRequest("POST", url, body)
 	if err != nil {
 		// The URL was ill-formed. Exit directly, it's not a common error
 		panic(err)
